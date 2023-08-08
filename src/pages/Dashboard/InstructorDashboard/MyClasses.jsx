4a724d72f1fd649ae916feb8a3ba582aeebf4b0c
MohammadNaiem65 <mohammadnaiem661065@gmail.com>
2023-08-09 04:56:11 +0600

    added a class delete functionality

--- src/pages/Dashboard/InstructorDashboard/MyClasses.jsx
+++ src/pages/Dashboard/InstructorDashboard/MyClasses.jsx
@@ -1,6 +1,7 @@
 import { useContext, useEffect, useState } from 'react';
 import { AuthContext } from './../../../Providers/AuthContextProvider/AuthContextProvider';
 import MyClass from './MyClass';
+import Swal from 'sweetalert2';
 
 const MyClasses = () => {
 	// ! Variable definitions
@@ -22,7 +23,29 @@ const MyClasses = () => {
 
 	// ! Handle remove a class
 	const handleRemoveClass = (id) => {
-		
+		fetch(`http://localhost:5000/instructor/classes/class/${id}`, {
+			method: 'DELETE',
+		})
+			.then((response) => response.json())
+			.then((data) => {
+				if (data.deletedCount > 0) {
+					Swal.fire(
+						'Successful!',
+						'Deleted Successfully!',
+						'success'
+					);
+					const existedClasses = [...myClasses].filter(
+						(c) => c._id !== id
+					);
+					setClasses(existedClasses);
+				} else {
+					Swal.fire({
+						icon: 'error',
+						title: 'Oops...',
+						text: 'Something went wrong!',
+					});
+				}
+			});
 	};
 
 	return (
